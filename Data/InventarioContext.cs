be2d66bf8a5e9c2fb3a7a2d686179db59da326c5
Modelo <adrianinfo499@gmail.com>
2026-03-03 13:55:58 -0600

    Add InventarioContext

---
+++ Data/InventarioContext.cs
@@ -0,0 +1,18 @@
+﻿using Inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Data
+{
+    public class InventarioContext : DbContext
+    {
+        public InventarioContext() : base("InventarioContext")
+        {
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
