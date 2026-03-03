fbdd0781e1eb97359649a7b8bc1c5ddb54f73a5a
Modelo <adrianinfo499@gmail.com>
2026-03-03 13:38:53 -0600

    Add Producto Entity

---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; } //Clave primaria
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public DateTime FechaIngreso { get;set; }
+        public bool Activo { get; set; }
+    }
+}
