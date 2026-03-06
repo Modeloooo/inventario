4f9411ef03ce8980b20cec47c87baf56a0619725
Modelo <adrianinfo499@gmail.com>
2026-03-05 18:15:50 -0600

    Configure Entity Framework and initial migration

---
+++ Migrations/202603060003074_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿namespace Inventario.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Productoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Descripcion = c.String(),
+                        Precio = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Stock = c.Int(nullable: false),
+                        FechaIngreso = c.DateTime(nullable: false),
+                        Activo = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Productoes");
+        }
+    }
+}
